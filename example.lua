60df10e2b16ce2ec80efd2f3e81830493a54bd10
nondevelopers <mozaireal@gmail.com>
2026-07-16 20:18:09 -0400

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,41 +1,49 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/nondevelopers/Vape-old-UiLibrary/main/vape-old.lua"))()
 
-local win = lib:Window("PREVIEW",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
+local win = lib:Window("PREVIEW", Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
 
 local tab = win:Tab("Tab 1")
 
 tab:Button("Button", function()
-lib:Notification("Notification", "Hello!", "Hi!")
+    lib:Notification("Notification", "Hello!", "Hi!")
 end)
 
-tab:Toggle("Toggle",false, function(t)
-print(t)
+local button2 = tab:Button("Button 2", function()
+    print("Button 2 pressed")
+end, true)
+
+button2:Label("Setting 1")
+button2:Label("Setting 2")
+button2:Label("Setting 3")
+
+tab:Toggle("Toggle", false, function(t)
+    print(t)
 end)
 
-tab:Slider("Slider",0,100,30, function(t)
-print(t)
+tab:Slider("Slider", 0, 100, 30, function(t)
+    print(t)
 end)
 
-tab:Dropdown("Dropdown",{"Option 1","Option 2","Option 3","Option 4","Option 5"}, function(t)
-print(t)
+tab:Dropdown("Dropdown", {"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"}, function(t)
+    print(t)
 end)
 
-tab:Colorpicker("Colorpicker",Color3.fromRGB(255,0,0), function(t)
-print(t)
+tab:Colorpicker("Colorpicker", Color3.fromRGB(255, 0, 0), function(t)
+    print(t)
 end)
 
-tab:Textbox("Textbox",true, function(t)
-print(t)
+tab:Textbox("Textbox", true, function(t)
+    print(t)
 end)
 
-tab:Bind("Bind",Enum.KeyCode.RightShift, function()
-print("Pressed!")
+tab:Bind("Bind", Enum.KeyCode.RightShift, function()
+    print("Pressed!")
 end)
 
 tab:Label("Label")
 
 local changeclr = win:Tab("Change UI Color")
 
-changeclr:Colorpicker("Change UI Color",Color3.fromRGB(44, 120, 224), function(t)
-lib:ChangePresetColor(Color3.fromRGB(t.R * 255, t.G * 255, t.B * 255))
+changeclr:Colorpicker("Change UI Color", Color3.fromRGB(44, 120, 224), function(t)
+    lib:ChangePresetColor(Color3.fromRGB(t.R * 255, t.G * 255, t.B * 255))
 end)
