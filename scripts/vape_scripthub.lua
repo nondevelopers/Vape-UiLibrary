5f600f6f119a4efa9b4317af142035ca022edd1e
nondevelopers <mozaireal@gmail.com>
2026-08-06 16:30:04 -0400

    Delete scripts/vape_scripthub.lua

--- scripts/vape_scripthub.lua
+++
@@ -1,8 +0,0 @@
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/nondevelopers/Vape-UiLibrary/main/source.lua"))()
-local win = lib:Window("Vape Scripthub", Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
--tabs
-local tab = win:Tab("Main")
-
-tab:Button("Button", function()
-    
-end)
